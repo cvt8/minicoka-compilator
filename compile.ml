2e6f63a29eafa5b2310d3446647c03098cdc7cde
CVT <constantin.tenzer@ens.psl.eu>
2025-01-19 17:42:56 +0100

    Allocation des expressions compile.ml

--- compile.ml
+++ compile.ml
@@ -7,26 +7,127 @@ open Typing
 
 (* Phase 1: allocation des variables *)
 
+(* Exception levée lorsqu'une variable n'est pas définie *)
 exception VarUndef of string
 
+(* Table de hachage globale pour stocker les variables *)
 let (genv : (string, unit) Hashtbl.t) = Hashtbl.create 17
 
+(* Module pour les maps de chaînes de caractères *)
 module Smap = Map.Make(String)
 
+(* Type pour l'environnement local, qui mappe les identifiants *)
 type local_env = ident Smap.t
 
+(* Fonction pour allouer les variables *)
+let rep_movsb = inline "rep movsb"
+
+let rec convert_expr = function
+  |Ast.Eblock b -> Typing.EBlock b
+  |Ast.Eexpr e -> Typing.EExpr e
+
+let rec alloc_expr = function
+  | EUnit -> EUnit
+  | EBool b -> EBool b
+  | EInt n -> EInt n
+  | EVar x -> EVar x
+  | EBinop (op, e1, e2) -> EBinop (op, alloc_expr e1, alloc_expr e2)
+  | EIf (e1, e2, e3) -> EIf (alloc_expr e1, alloc_expr e2, alloc_expr e3)
+  | EElse (e1, e2) -> EElse (alloc_expr e1, alloc_expr e2)
+  | EFn (Fbody (params, annot_opt, expr)) -> EFn (Fbody (params, annot_opt, expr))
+  | EFun (Fbody (params, annot_opt, expr)) -> EFun (Fbody (params, annot_opt, expr))
+  | EReturn e -> EReturn (alloc_expr e)
+  | EThen (e1, e2) -> EThen (alloc_expr e1, alloc_expr e2)
+  | EVal e -> EVal (alloc_expr e)
+  | EElif (e1, e2, e3) -> EElif (alloc_expr e1, alloc_expr e2, alloc_expr e3)
+  | EString s -> EString s
+  | ELet (x, e1, e2) -> ELet (x, alloc_expr e1, alloc_expr e2)
+  | EApp (e, es) -> EApp (alloc_expr e, List.map alloc_expr es)
+  | EPrintln e -> EPrintln (alloc_expr e)
+  | EFor (e1, e2, e3) -> EFor (alloc_expr e1, alloc_expr e2, alloc_expr e3)
+  | ERepeat (e1, e2) -> ERepeat (alloc_expr e1, alloc_expr e2)
+  | EWhile (e1, e2) -> EWhile (alloc_expr e1, alloc_expr e2)
+
+(* Fonction pour allouer les variables d'une expression *)
+
+let alloc (p: Ast.file) = List.map (fun d -> alloc_expr d) p
+
+
+(******************************************************************************)
+(* phase 2 : production de code *)
+
+(* Fonction récursive pour compiler une expression *)
 let rec compile_expr = function
-  | EUnit -> nop
-  | EBool b -> movq (imm (if b then 1 else 0)) !%rax
-  | EInt n -> movq (imm n) !%rax
-  | EVar x ->
+  | EUnit -> nop  (* Expression unité *)
+  | EBool b -> movq (imm (if b then 1 else 0)) !%rax  (* Expression booléenne *)
+  | EInt n -> movq (imm n) !%rax  (* Expression entière *)
+  | EVar x ->  (* Expression variable *)
     begin
       try movq (lab x) !%rax
       with Not_found -> raise (VarUndef x)
     end
-  | EBinop (op, e1, e2) -> compile_binop op e1 e2
-  | EIf (e1, e2, e3) -> compile_if e1 e2 e3
+  | EBinop (op, e1, e2) -> compile_binop op e1 e2  (* Expression binaire *)
+  | EIf (e1, e2, e3) -> compile_if e1 e2 e3  (* Expression conditionnelle *)
+  | EElse (e1, e2) -> compile_expr e1 ++ compile_expr e2  (* Expression séquentielle *)
+  | EFn (Fbody (_, _, _)) -> failwith "Unsupported function type"  (* Expression fonction *)
+  | EFun (Fbody (_, _, _)) -> failwith "Unsupported function type"  (* Expression fonction *)
+  | EReturn e -> compile_expr e  (* Expression return *)
+  | EThen (e1, e2) -> compile_expr e1 ++ compile_expr e2  (* Expression séquentielle *)
+  | EVal e -> compile_expr e  (* Expression valeur *)
+  | EElif (e1, e2, e3) -> compile_expr e1 ++ compile_expr e2 ++ compile_expr e3  (* Expression elif *)
+  | EString s ->  (* Expression chaîne de caractères *)
+    let l = ".S" ^ string_of_int (Hashtbl.length genv) in
+    Hashtbl.add genv l ();
+    movq (ilab l) !%rax ++
+    movq (imm (String.length s)) !%rdi ++
+    call "malloc" ++
+    movq !%rax (lab l) ++
+    movq (imm 0) !%rdi ++
+    movq (imm (String.length s)) !%rcx ++
+    movq (ilab s) !%rsi ++
+    rep_movsb ++
+    movq (lab l) !%rax
+  | ELet (x, e1, e2) ->  (* Expression let *)
+    compile_expr e1 ++
+    pushq !%rax ++
+    compile_expr e2 ++
+    popq rdi ++
+    movq !%rax (lab x) ++
+    movq !%rdi !%rax
+  | EApp (e, es) ->  (* Expression application *)
+    let code = List.fold_left (fun acc e -> acc ++ compile_expr e ++ pushq !%rax) nop es in
+    code ++ compile_expr e ++ call "*%rax" ++ inline "addq $8, %rsp"
+  | EPrintln e ->  (* Expression println *)
+    compile_expr e ++
+    movq !%rax !%rdi ++
+    call "print_int" ++
+    movq (imm 0) !%rax
+  | EFor (e1, e2, e3) ->  (* Expression for *)
+    compile_expr e1 ++
+    label "for" ++
+    compile_expr e2 ++
+    testq !%rax !%rax ++
+    jz "endfor" ++
+    compile_expr e3 ++
+    jmp "for" ++
+    label "endfor"
+  | ERepeat (e1, e2) ->  (* Expression repeat *)
+    label "repeat" ++
+    compile_expr e1 ++
+    testq !%rax !%rax ++
+    jnz "repeat" ++
+    compile_expr e2
+  | EWhile (e1, e2) ->  (* Expression while *)
+    label "while" ++
+    compile_expr e1 ++
+    testq !%rax !%rax ++
+    jz "endwhile" ++
+    compile_expr e2 ++
+    jmp "while" ++
+    label "endwhile"
+  
 
+(* Fonction pour compiler une opération binaire *)
 and compile_binop op e1 e2 =
   let code1 = compile_expr e1 in
   let code2 = compile_expr e2 in
@@ -46,12 +147,15 @@ and compile_binop op e1 e2 =
   | ">=" -> code1 ++ pushq !%rax ++ code2 ++ popq rbx ++ cmpq !%rbx !%rax ++ setge !%al ++ movzbq !%al rax
   | _ -> failwith "Opérateur binaire non supporté"
 
+
+(* Fonction pour compiler une expression conditionnelle *)
 and compile_if e1 e2 e3 =
   let code1 = compile_expr e1 in
   let code2 = compile_expr e2 in
   let code3 = compile_expr e3 in
   code1 ++ testq !%rax !%rax ++ jz "else" ++ code2 ++ jmp "end" ++ label "else" ++ code3 ++ label "end"
 
+(* Fonction pour compiler le corps d'une fonction *)
 and compile_funbody = function
   | Fbody (params, annot_opt, expr) ->
       let allocate_closure =
@@ -68,10 +172,17 @@ and compile_funbody = function
         inline "call *%rax" ++
         inline "addq $8, %rsp"
       in
-      let typed_expr = Typing.type_expr [] (Typing.convert_expr expr) in
-      allocate_closure ++ initialize_closure ++ compile_expr typed_expr ++ call_closure
+      let convert_param_to_pair = function
+        | Pdots (x, t) -> (x, convert_param_type t)
+        | PFn (Fbody (_, _, _)) -> failwith "Unsupported parameter type"
+      in
+      let typed_params = List.map convert_param_to_pair params in
+      let typed_expr = Typing.type_expr typed_params (convert_expr expr) in
+      allocate_closure ++ initialize_closure ++ compile_expr (convert_expr expr) ++ call_closure
 
-let compile_program p ofile =
+
+(* Fonction pour compiler un programme complet *)
+let compile_program (p: Ast.file) (ofile: string) =
   let p = alloc p in
   Format.eprintf "%a@." print p;
   let codefun, code = List.fold_left compile_stmt (nop, nop) p in
--- typing.ml
+++ typing.ml
@@ -21,58 +21,58 @@ and value_type =
   | TList of value_type (* Type liste paramétré par le type des éléments *)
   | Tmaybe of value_type (* Type optionnel paramétré par le type de la valeur *)
   | Tfunc of value_type list * calculation_type (* Type fonction avec liste de types d'arguments et type de retour *)
+  | Tfuncs of value_type * (string * value_type) list (* Type fonction avec liste de types d'arguments et type de retour *)
 
 (* Définition des expressions possibles *)
 and expr =
-    | EUnit (* Expression unité *)
-    | EBool of bool (* Expression booléenne *)
-    | EInt of int (* Expression entière *)
-    | EString of string (* Expression chaîne de caractères *)
-    | EVar of string (* Expression variable *)
-    | EBinop of string * expr * expr (* Expression binaire avec opérateur et deux sous-expressions *)
-    | EIf of expr * expr * expr (* Expression conditionnelle if *)
-    | ELet of string * expr * expr (* Expression let *)
-    | EList of expr list (* Expression liste *)
-    | EFunc of string list * value_type list * expr (* Expression fonctionnelle *)
-    | EApp of expr * expr list (* Expression d'application de fonction *)
-    | EPrintln of expr (* Expression println *)
-    | EHead of expr (* Expression head pour liste *)
-    | ETail of expr (* Expression tail pour liste *)
-    | EDefault of expr * expr (* Expression default pour valeurs optionnelles *)
-    | EFor of expr * expr * expr (* Expression for *)
-    | ERepeat of expr * expr (* Expression repeat *)
-    | EWhile of expr * expr (* Expression while *)
+  | EUnit (* Expression unité *)
+  | EBool of bool (* Expression booléenne *)
+  | EInt of int (* Expression entière *)
+  | EString of string (* Expression chaîne de caractères *)
+  | EVar of string (* Expression variable *)
+  | EBinop of string * expr * expr (* Expression binaire avec opérateur et deux sous-expressions *)
+  | EIf of expr * expr * expr (* Expression conditionnelle if *)  
+  | ELet of string * expr * expr (* Expression let *)
+  | EList of expr list (* Expression liste *)
+  | EFunc of string list * value_type list * expr (* Expression fonctionnelle *)
+  | EApp of expr * expr list (* Expression d'application de fonction *)
+  | EPrintln of expr (* Expression println *)
+  | EHead of expr (* Expression head pour liste *)
+  | ETail of expr (* Expression tail pour liste *)
+  | EDefault of expr * expr (* Expression default pour valeurs optionnelles *)
+  | EFor of expr * expr * expr (* Expression for *)
+  | ERepeat of expr * expr (* Expression repeat *)
+  | EWhile of expr * expr (* Expression while *)
+  | EElse of expr * expr (* Expression else *)
+  | EReturn of expr (* Expression return *)
+  | EThen of expr * expr (* Expression then *)
+  | EVal of expr (* Expression valeur *)
+  | EElif of expr * expr * expr (* Expression elif *)
+  | EFn of funbody (* Expression fonction *)
+  | EFun of funbody (* Expression fonction *)
+  | EBlock of block (* Expression bloc *)
+  | EExpr of bexpr (* Expression bexpr *)
 
-(* Fonction pour convertir un type de valeur en chaîne de caractères *)
-let rec string_of_value_type = function
-  | Tunit -> "unit"
-  | Tbool -> "bool"
-  | Tint -> "int"
-  | Tstring -> "string"
-  | TList t -> "list of " ^ string_of_value_type t
-  | Tmaybe t -> "maybe " ^ string_of_value_type t
-  | Tfunc (args, (ret, eff)) ->
-      "(" ^ String.concat " * " (List.map string_of_value_type args) ^ ") -> " ^ string_of_value_type ret
 
-(* Fonction pour combiner deux effets *)
-let combine_effect(eff, eff2) =
-  match eff with
+(* Fonction pour combiner les effets *)
+let combine_effect eff1 eff2 =
+  match eff1 with
   | Nothing -> eff2
-  | Div -> match eff2 with
-    | Nothing -> Div
-    | Div -> Div
-    | Console -> Div_and_Console
-    | Div_and_Console -> Div_and_Console
-  | Console -> match eff2 with
-    | Nothing -> Console
-    | Div -> Div_and_Console
-    | Console -> Console
-    | Div_and_Console -> Div_and_Console
+  | Div -> (match eff2 with
+            | Nothing -> Div
+            | Div -> Div
+            | Console -> Div_and_Console
+            | Div_and_Console -> Div_and_Console)
+  | Console -> (match eff2 with
+                | Nothing -> Console
+                | Div -> Div_and_Console
+                | Console -> Console
+                | Div_and_Console -> Div_and_Console)
   | Div_and_Console -> Div_and_Console
-  | _ -> failwith "Unexpected effect"
+  
 
 (* Fonctions pour convertir une expression de la syntaxe abstraite en expression de notre langage *)
-
+(* Fonctions pour convertir une expression de la syntaxe abstraite en expression de notre langage *)
 let rec convert_stmt = function
   | Sblock s -> convert_block s
   | _ -> failwith "Not implemented"
@@ -83,36 +83,42 @@ and convert_block = function
 
 and convert_param = function
   | Pdots (x, t) -> (x, convert_param_type t)
+  | PFn (Fbody (params, annot_opt, expr)) -> ("", Tfunc (List.map convert_param_to_param_type params, convert_result annot_opt))
+
+and convert_param_to_param_type = function
+  | Pdots (x, t) -> convert_param_type t
 
 and convert_param_type = function
   | PBase t -> convert_atype t
-  | PArrow (t, r) -> Tfunc ([convert_atype t], convert_result r)
-  | PArrowpar (params, r) -> Tfunc (List.map convert_param_type params, convert_result r)
+  | PArrow (t, r) -> Tfunc ([convert_atype t], convert_result (Some r))
+  | PArrowpar (params, r) -> Tfunc (List.map convert_param_type params, convert_result (Some r))
   | PAFn f -> Tfunc ([], convert_funbody f)
+  (*| PAnnot (idents, t) -> Tfunc ([convert_atype t], convert_result (Some (idents, Some t))) *)
 
 and convert_atype = function
-  | ATypeApp (x, params) -> TList (List.map convert_param_type params)
+  | ATypeApp (x, params) -> Tfunc (List.map convert_param_type params, (Tunit, Nothing))
   | ATypeParen t -> convert_atype t
   | AUnit -> Tunit
 
 and convert_result = function
-  | ([], Some t) -> convert_param_type t
-  | (idents, Some t) -> Tfunc (List.map (fun _ -> Tunit) idents, convert_param_type t)
-  | ([], None) -> Tunit
-  | _ -> failwith "Not implemented"
+  | None -> (Tunit, Nothing)
+
 
 and convert_funbody = function
-  | Fbody (params, annot_opt, expr) -> EFunc (List.map (fun (Pdots (x, _)) -> x) params, List.map (fun (Pdots (_, t)) -> convert_param_type t) params, convert_expr expr)
+  | Fbody (params, annot_opt, expr) ->
+      let param_types = List.map convert_param_to_param_type params in
+      let body_type = convert_expr expr in
+      (Tfunc (param_types, (body_type, Nothing)), Nothing)
 
 and convert_expr = function
   | Eblock b -> convert_block b
   | Eexpr e -> convert_bexpr e
-  | Call (e, params) -> EApp (convert_expr e, List.map convert_param params)
-  | Fn e -> EFunc ([], [], convert_expr e)
-  | Block stmts -> EList (List.map convert_stmt stmts)
+  | Call (e, params) -> Tfuncs (convert_expr e, List.map convert_param params)
+  (*| Fn e -> Tfunc ( [], convert_expr e)
+  | Block stmts -> TList ( convert_stmt stmts) 
   | ECall (a, params) -> EApp (convert_atom a, List.map convert_expr params)
   | ECallb (e, params) -> EApp (convert_expr e, List.map convert_expr params)
-  | AeCall (a, params) -> EApp (convert_atom a, List.map convert_expr params)
+  | AeCall (a, params) -> EApp (convert_atom a, List.map convert_expr params) *)
 
 and convert_atom = function
   | AIdent x -> EVar x
@@ -121,29 +127,15 @@ and convert_atom = function
   | ATrue -> EBool true
   | AFalse -> EBool false
   | AUnit -> EUnit
-  | AParen (Some e) -> convert_expr e
+ (* | AParen (Some e) -> convert_expr e *)
   | AParen None -> EUnit
-  | ACall (a, params) -> EApp (convert_atom a, List.map convert_expr params)
+  (*| ACall (a, params) -> EApp (convert_atom a, List.map convert_expr params) *)
   | ADot (a, x) -> EVar x
-  | AFun f -> EFunc ([], [], convert_bexpr (BFun f))
-  | ABlock b -> convert_block b
-
+ (* | AFun f -> EFunc ([], [], convert_bexpr (BFun f)) *)
+ (* | ABlock b -> convert_block b *)
+    
 and convert_bexpr = function
-  | BAtom a -> convert_atom a
-  | BNot e -> EBinop ("!", convert_bexpr e, EUnit)
-  | BNeg e -> EBinop ("-", convert_bexpr e, EUnit)
-  | BBinop (e1, op, e2) -> EBinop (op, convert_bexpr e1, convert_bexpr e2)
-  | BAssign (x, e) -> ELet (x, convert_bexpr e, EVar x)
-  | BIf (e1, e2, None) -> EIf (convert_bexpr e1, convert_expr e2, EUnit)
-  | BIf (e1, e2, Some e3) -> EIf (convert_bexpr e1, convert_expr e2, convert_expr e3)
-  | BIfElse (e1, e2, [], None) -> EIf (convert_bexpr e1, convert_expr e2, EUnit)
-  | BIfElse (e1, e2, [], Some e3) -> EIf (convert_bexpr e1, convert_expr e2, convert_expr e3)
-  | BIfElse (e1, e2, (e3, e4) :: elifs, else_) ->
-      EIf (convert_bexpr e1, convert_expr e2, Some (EIf (convert_bexpr e3, convert_expr e4, convert_bexpr (BIfElse (EAtom (AUnit), EBlock (Sblocks elifs), else_)))))
-  | BIfReturn (e1, e2) -> EIf (convert_bexpr e1, convert_expr e2, EUnit)
-  | BFun f -> EFunc ([], [], convert_bexpr (BFun f))
-  | BReturn e -> ELet ("_", convert_expr e, EVar "_")
-
+    | BReturn e -> convert_expr e
 
 
 (* Fonction principale de typage des expressions *)
@@ -161,21 +153,21 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
       let (t2, eff2) = type_expr ctx e2 in
       begin
         match op, t1, t2 with
-        | ("+" | "-" | "*" | "/" | "%"), Tint, Tint -> (Tint, combine_effect(eff1, eff2))
-        | ("<" | ">" | "<="| ">="), Tint, Tint -> (Tbool, combine_effect(eff1, eff2))
-        | ("&&"| "||"), Tbool, Tbool -> (Tbool, combine_effect(eff1, eff2))
-        | ("++"), Tstring, Tstring -> (Tstring, combine_effect(eff1, eff2))
-        | ("++"), TList t1, TList t2 when t1 = t2 -> (TList t1, combine_effect(eff1, eff2))
-        | ("=="| "!="), Tbool, Tbool -> (Tbool, combine_effect(eff1, eff2))
-        | ("=="| "!="), Tint, Tint -> (Tint, combine_effect(eff1, eff2))
-        | ("=="| "!="), Tstring, Tstring -> (Tstring, combine_effect(eff1, eff2))
+        | ("+" | "-" | "*" | "/" | "%"), Tint, Tint -> (Tint, combine_effect eff1 eff2)
+        | ("<" | ">" | "<="| ">="), Tint, Tint -> (Tbool, combine_effect eff1 eff2)
+        | ("&&"| "||"), Tbool, Tbool -> (Tbool, combine_effect eff1 eff2)
+        | ("++"), Tstring, Tstring -> (Tstring, combine_effect eff1 eff2)
+        | ("++"), TList t1, TList t2 when t1 = t2 -> (TList t1, combine_effect eff1 eff2)
+        | ("=="| "!="), Tbool, Tbool -> (Tbool, combine_effect eff1 eff2)
+        | ("=="| "!="), Tint, Tint -> (Tint, combine_effect eff1 eff2)
+        | ("=="| "!="), Tstring, Tstring -> (Tstring, combine_effect eff1 eff2)
         | _ -> failwith "Opération binaire non supportée ou types incompatibles"
       end
   | EIf (e1, e2, e3) ->
       let (t1, eff1) = type_expr ctx e1 in
       let (t2, eff2) = type_expr ctx e2 in
       let (t3, eff3) = type_expr ctx e3 in
-      if t1 = Tbool && t2 = t3 then (t2, combine_effect(combine_effect(eff1, eff2), eff3))
+      if t1 = Tbool && t2 = t3 then (t2, combine_effect eff1 (combine_effect eff2 eff3))
       else failwith "Condition mal typée ou branches de types différents"
   | ELet (x, e1, e2) ->
       let (t1, eff1) = type_expr ctx e1 in
@@ -186,7 +178,7 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
       | e1 :: _ ->
           let (t1, eff1) = type_expr ctx e1 in
           List.iter (fun e -> if fst (type_expr ctx e) <> t1 then failwith "Liste mal typée") es;
-          (TList t1, List.fold_left (fun eff e -> combine_effect(snd (type_expr ctx e), eff)) Nothing es)
+          (TList t1, List.fold_left (fun eff e -> combine_effect (snd (type_expr ctx e)) eff) Nothing es)
       end
   | EFunc (args, arg_types , body) ->
       let (t_body, eff_body) = type_expr (List.combine args arg_types @ ctx) body in
@@ -196,13 +188,13 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
       begin match t_f with
       | Tfunc (arg_types, (t_ret, eff_ret)) ->
           let arg_types2 = List.map (fun arg -> fst (type_expr ctx arg)) args in
-          if arg_types = arg_types2 then (t_ret, List.fold_left (fun eff e -> combine_effect(snd (type_expr ctx e), eff)) eff_ret args)
+          if arg_types = arg_types2 then (t_ret, List.fold_left (fun eff e -> combine_effect (snd (type_expr ctx e)) eff) eff_ret args)
           else failwith "Types des arguments de la fonction incorrects"
       | _ -> failwith "Tentative d'application d'une expression non fonctionnelle"
       end
   | EPrintln e1 -> let (t, eff) = type_expr ctx e1 in
     if List.mem t [Tunit; Tbool; Tint; Tstring] then
-      (Tunit, combine_effect(eff, Console))
+      (Tunit, combine_effect eff Console)
     else failwith "println : type non supporté"
   | EHead e1 ->
     let (t, eff) = type_expr ctx e1 in
@@ -220,7 +212,7 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
     let (t1, eff1) = type_expr ctx e1 in
     let (t2, eff2) = type_expr ctx e2 in
     (match t1 with
-      | Tmaybe t3 -> if t1 = t2 then (t1, combine_effect(eff1, eff2))
+      | Tmaybe t3 -> if t1 = t2 then (t1, combine_effect eff1 eff2)
         else failwith "default : types incompatibles"
       | _ -> failwith "default : types incompatibles")
   | EFor (e1, e2, e3) ->
@@ -229,7 +221,7 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
     let (t3, eff3) = type_expr ctx e3 in
     if t1 = t2 && t1 = Tint then
       (match t3 with
-        | Tfunc ([Tint], (Tunit, eff4)) -> (Tunit, combine_effect(eff4, combine_effect(eff3, combine_effect(eff1, eff2))))
+        | Tfunc ([Tint], (Tunit, eff4)) -> (Tunit, combine_effect eff4 (combine_effect eff3 (combine_effect eff1 eff2)))
         | _ -> failwith "for : mauvais troisieme argument")
     else failwith "for : premier et deuxieme argument doit être un entier"
   | ERepeat (e1, e2) ->
@@ -237,7 +229,7 @@ let rec type_expr (ctx : (string * value_type) list) (e : expr) : calculation_ty
     let (t2, eff2) = type_expr ctx e2 in
     if t1 = Tint then
       (match t2 with
-        | Tfunc ([], (Tunit, eff3)) -> (Tunit, combine_effect(eff3, combine_effect(eff1, eff2)))
+        | Tfunc ([], (Tunit, eff3)) -> (Tunit, combine_effect eff3 (combine_effect eff1 eff2))
         | _ -> failwith "repeat : mauvais deuxieme argument")
     else failwith "repeat : premier argument doit être un entier"
   | EWhile (e1, e2) ->
